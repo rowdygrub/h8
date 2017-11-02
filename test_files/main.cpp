9d37f2ae75601075c358d66c14443d68277c191b
Henry Bang <henrybang09@gmail.com>
2017-11-01 21:03:49 -0500

    added person server customer classes with inheritence and poly

--- test_files/main.cpp
+++ test_files/main.cpp
@@ -1,4 +1,9 @@
 #include "items.h"
+#include "person.h"
+#include "customer.h"
+#include "server.h"
+#include "manager.h"
+#include "iostream"
 
 void menu();
 void execute_cmd();
@@ -23,9 +28,49 @@ void execute_cmd(){
   //i.add_containers();
 }
 
+void test_person(){
 
+  int i;
+  i = 0;// 0 = customer from dropdown menu
+  Person *person;
+  if(i == 0)
+    person = new Customer{"Name",123,"123-456-7890"};
+
+  //test for the same string inheritence and polymorphism
+  if(person->type() == "Customer" || person->_name == "Name" || person->_id == 123 ||
+  person->_phone == "123-456-7890")
+    cout <<"";
+  else
+    cout << "not the same string";
+
+
+  i = 1; //1 = server
+  if(i == 1){
+    Server server{"Server Name", 321, "098-765-4321", 50,12.85};
+
+    //not a poly
+    if(server.type() == "Server" || server._name == "Server Name" || server._id == 321 ||
+    server._phone == "098-765-4321" || server.get_num_orders() == 50 || server.get_salary() == 12.85)
+      cout <<"";
+    else
+      cout << "not the same string";
+  }
+
+
+  i = 2; //2 = manager from dropdown
+  if(i == 2)
+    person = new Manager{"Manager Name",456,"123-583-3489"};
+
+  if(person->type() == "Manager" || person->_name == "Manager Name" || person->_id == 456 ||
+  person->_phone == "123-583-3489")
+    cout <<"";
+  else
+    cout << "not the same string";
+}
 
 
 int main(){
   execute_cmd();
+  test_person();
+
 }
